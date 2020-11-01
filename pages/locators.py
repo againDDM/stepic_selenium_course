d86feddf060d24fc7c97d9a77244da5dc2e25f1c
againDDM <againddm@gmail.com>
2020-11-01 23:20:04 +0300

    parametrize product page test by promo

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,12 @@ class MainPageLocators():
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+
+
+class ProductPageLocators():
+    PRODUCT_URL = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    ADD_PRODUCT_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    ALERT_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages div.alert-success div.alertinner strong")
+    ALERT_PRODUCT_PRICE = (By.CSS_SELECTOR, "#messages div.alert-info div.alertinner strong")
+    PAGE_PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    PAGE_PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main p.price_color")
---
+++ pages/product_page.py
@@ -0,0 +1,30 @@
+from .base_page import BasePage   # pylint: disable=relative-beyond-top-level
+from .locators import ProductPageLocators   # pylint: disable=relative-beyond-top-level
+from selenium.webdriver.common.by import By
+
+
+class ProductPage(BasePage):
+
+    def add_product_to_basket(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_PRODUCT_TO_BASKET_BUTTON)
+        button.click()
+
+
+    def check_product_name(self):
+        page_product_name = self.browser.find_element(
+            *ProductPageLocators.PAGE_PRODUCT_NAME
+        ).text
+        alert_product_name =self.browser.find_element(
+            *ProductPageLocators.ALERT_PRODUCT_NAME
+        ).text
+        assert page_product_name == alert_product_name, "Incorrect product name"
+
+
+    def check_product_price(self):
+        page_product_price = self.browser.find_element(
+            *ProductPageLocators.PAGE_PRODUCT_PRICE
+        ).text
+        alert_product_price =self.browser.find_element(
+            *ProductPageLocators.ALERT_PRODUCT_PRICE
+        ).text
+        assert page_product_price == alert_product_price, "Incorrect product price"
