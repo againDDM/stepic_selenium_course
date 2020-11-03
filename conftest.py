b32033092b0beaff43a53bb211965711283fa143
againDDM <againddm@gmail.com>
2020-11-03 23:00:56 +0300

    inheritance compleate

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ def pytest_addoption(parser):
     )
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def browser(request):
     options = Options()
     options.add_experimental_option(
@@ -21,8 +21,3 @@ def browser(request):
     browser = webdriver.Chrome()
     yield browser
     browser.quit()
-
-
-@pytest.fixture(scope="function", autouse=True)
-def clear_cookie(browser):
-    browser.delete_all_cookies()
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 from .pages.product_page import ProductPage   # pylint: disable=relative-beyond-top-level
 from .pages.locators import ProductPageLocators   # pylint: disable=relative-beyond-top-level
+from .pages.login_page import LoginPage   # pylint: disable=relative-beyond-top-level
 
 
 @pytest.mark.parametrize(
@@ -27,3 +28,62 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.check_product_name()
     page.check_product_price()
+
+
+@pytest.mark.xfail(reason="I was forced to write this falling test.")
+@pytest.mark.parametrize(
+    'link', [
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/",
+        "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/",
+    ]
+)
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    assert page.is_not_element_present(*ProductPageLocators.ALERT_PRODUCT_NAME), \
+        "as it should be"
+
+
+@pytest.mark.parametrize(
+    'link', [
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/",
+        "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/",
+    ]
+)
+def test_guest_cant_see_success_message(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    assert page.is_not_element_present(*ProductPageLocators.ALERT_PRODUCT_NAME), \
+        "success message should not appear immediately after opening the page"
+
+
+@pytest.mark.xfail(reason="I was forced to write this falling test.")
+@pytest.mark.parametrize(
+    'link', [
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/",
+        "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/",
+    ]
+)
+def test_message_disappeared_after_adding_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    assert page.is_disappeared(*ProductPageLocators.ALERT_PRODUCT_NAME), \
+        "as it should be"
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
